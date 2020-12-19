60d4a0045f1a3f9f8002a409555ba69861921b2d
Ömer Faruk Şahin <faruk_thecno@hotmail.com>
2020-12-19 21:23:19 +0300

    #7

---
+++ src/WFEngine.Activities.Basic/Assign.cs
@@ -0,0 +1,12 @@
+﻿using WFEngine.Activities.Core;
+
+namespace WFEngine.Activities.Basic
+{
+    public class Assign : WFActivity
+    {
+        public override WFResponse Run()
+        {
+            return new WFResponse();
+        }
+    }
+}
---
+++ src/WFEngine.Activities.Basic/Container.cs
@@ -0,0 +1,14 @@
+﻿using WFEngine.Activities.Core;
+using WFEngine.Activities.Core.Helper;
+
+namespace WFEngine.Activities.Basic
+{
+    public class Container : WFActivity
+    {      
+        public override WFResponse Run()
+        {
+            Current.Blocks.RunBlock(Variables);
+            return new WFResponse();
+        }
+    }
+}
